0576cc47b30f6e3ed9962b20e32b9e9d974fdab1
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-05-04 08:54:58 -0500

    fix(back/package.json): Fixed unknown file extension bug

--- server/src/createDatabaseConnection.ts
+++ server/src/createDatabaseConnection.ts
@@ -1,12 +1,16 @@
 import { createConnection, Connection } from "typeorm";
 import models from "./models";
 
-const connection: Connection = await createConnection({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "ijalti_test",
-    entities: models,
-});
+const createDatabaseConnection = async () => {
+    const connection: Connection = await createConnection({
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "postgres",
+        password: "password",
+        database: "ijalti_test",
+        entities: models,
+    });
+};
+
+export default createDatabaseConnection;
