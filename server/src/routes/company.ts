562f3ba03c33ff37e28ef27749c2851fae8b3b45
BrandonMagana <magana.brandon12@outlook.com>
2022-05-05 19:03:20 -0500

    Created getCompany and removeCompany endpoints

--- server/src/routes/company.ts
+++ server/src/routes/company.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { getCompanies, createCompany } from "../controllers";
+import { getCompaniesList, createCompany, getCompany, destroyCompany} from "../controllers";
 
 const companiesRouter = Router();
-const companyList = companiesRouter.route("/companies")
 
-companyList.get(getCompanies);
+const companyList = companiesRouter.route("/companies")
+const companyDetail = companiesRouter.route("/companies/:id")
+
+companyList.get(getCompaniesList);
 companyList.post(createCompany);
+
+companyDetail.get(getCompany);
+companyDetail.delete(destroyCompany);
 
 export default companiesRouter;
