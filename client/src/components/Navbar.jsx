839a5484c612f2d40c36f6534048ba77bf44d90c
CarlosCRG19 <carloscrg19@gmail.com>
2022-05-10 17:21:02 -0500

    feat(frontend): add linter to project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import MenuItem from '@mui/material/MenuItem'
-import AppBar from '@mui/material/AppBar'
-import Container from '@mui/material/Container'
-import Toolbar from '@mui/material/Toolbar'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuItem from '@mui/material/MenuItem';
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
 
-const Navbar = () => {
-	return (
-		<nav className='navbar'>
-			<div className='navbar-logo'>
-				<div className='navbar-icon'></div>
-				<h1>IJALTI PEERS</h1>
-			</div>
-		</nav>
-	)
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <div className="navbar-icon" />
+        <h1>IJALTI PEERS</h1>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
