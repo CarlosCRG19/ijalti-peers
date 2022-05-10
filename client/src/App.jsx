156191198e6b89178ac504753a66f6bfdc9ac53f
CarlosCRG19 <carloscrg19@gmail.com>
2022-05-10 17:21:02 -0500

    feat(frontend): add linter to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import Navbar from './components/Navbar'
-import Form from './components/Form'
-import './App.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-		<>	
-			<Navbar></Navbar>
-			<Form></Form>
-		</>
-  )
+    <>
+      <Navbar />
+      <Form />
+    </>
+  );
 }
 
-export default App
+export default App;
