2f66e419e4c97894e6a172b47d46a6d3ef02426e
CarlosCRG19 <carloscrg19@gmail.com>
2022-05-13 17:13:03 -0500

    feat(devops): add environment variables

--- server/src/createDatabaseConnection.ts
+++ server/src/createDatabaseConnection.ts
@@ -4,11 +4,11 @@ import models from "./models";
 const createDatabaseConnection = async () => {
     const connection: Connection = await createConnection({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: "postgres",
-        password: "cisco",
-        database: "ijalti_test",
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         entities: models,
         synchronize: true,
     });
