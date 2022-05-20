729d758d849df99895ba76a1b2e9cd0c1cd7e926
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-05-19 19:06:42 -0500

    feat(server/models): Create Aspirant model

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,6 @@
 import Company from "./company";
 import JobOffer from "./jobOffer";
 import User from "./user";
+import Aspirant from "./aspirant";
 
-export default [Company, JobOffer, User]
+export default [Company, JobOffer, User, Aspirant]
