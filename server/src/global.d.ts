281edf54dd7a84b2c39f63e1feec8f7071238a77
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-05-20 12:56:36 -0500

    feat(server/env): Add  ProcessEnv types

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -1,6 +1,11 @@
 namespace NodeJS {
     interface ProcessEnv {
-        NODE_ENV: string;
         PORT: string;
+        FIREBASE_API_KEY: string;
+        DB_HOST: string;
+        DB_PORT: string;
+        DB_USERNAME: string;
+        DB_PASSWORD: string;
+        DB_DATABASE: string;
     }
 }
