32ba1a83fcea5df6acaf87d15104c14dcb1623fd
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-05-21 16:55:47 -0500

    feat(server/controllers): Create login controller

--- server/src/routes/company.ts
+++ server/src/routes/company.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { getCompaniesList, createCompany, getCompany, removeCompany, updateCompany, companySignUp} from "../controllers";
+import { getCompaniesList, createCompany, getCompany, removeCompany, updateCompany, companySignUp, companyLogIn} from "../controllers";
 
 const companiesRouter = Router();
 
 const companyList = companiesRouter.route("/companies");
 const companyDetail = companiesRouter.route("/companies/:id");
-companiesRouter.post("/companies/signup", companySignUp);
 
 companyList.get(getCompaniesList);
 companyList.post(createCompany);
@@ -13,5 +12,8 @@ companyList.post(createCompany);
 companyDetail.get(getCompany);
 companyDetail.delete(removeCompany);
 companyDetail.put(updateCompany)
+
+companiesRouter.post("/companies/signup", companySignUp);
+companiesRouter.post("/companies/login", companyLogIn);
 
 export default companiesRouter;
