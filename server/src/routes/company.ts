18461bde5a93aef8010472579435b9cdccc6906f
Antonio C. Monroy <tony_monroe1212@hotmail.com>
2022-05-21 11:59:19 -0500

    fix(server/controllers): Add POST  method to firebase request

--- server/src/routes/company.ts
+++ server/src/routes/company.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getCompaniesList, createCompany, getCompany, removeCompany, updateCompany} from "../controllers";
+import { getCompaniesList, createCompany, getCompany, removeCompany, updateCompany, companySignUp} from "../controllers";
 
 const companiesRouter = Router();
 
-const companyList = companiesRouter.route("/companies")
-const companyDetail = companiesRouter.route("/companies/:id")
+const companyList = companiesRouter.route("/companies");
+const companyDetail = companiesRouter.route("/companies/:id");
+companiesRouter.post("/companies/signup", companySignUp);
 
 companyList.get(getCompaniesList);
 companyList.post(createCompany);
