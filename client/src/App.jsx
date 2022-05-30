022b1fe7606d9f5ad659fbef629b47cca8493a66
CarlosCRG19 <carloscrg19@gmail.com>
2022-05-30 16:11:34 -0500

    feat(client/tests): Added unit tests for PostJobOffer view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Navbar from './components/Navbar';
+import { Navbar } from './components';
 import PostJobOffer from './views/PostJobOffer';
 
 import './App.css';
