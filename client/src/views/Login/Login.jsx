3a8d0afc04fd8ffe133ce4133c646c25ce5d2333
DavBot02 <davidlopezsth@gmail.com>
2022-06-05 20:36:39 -0500

    feat/fix(front): add routes and fix style issues

--- client/src/views/Login/Login.jsx
+++ client/src/views/Login/Login.jsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
+  useTheme,
   Button,
   ButtonGroup,
+  Card,
+  CardContent,
   Grid,
-  TextField,
   Typography,
+  Alert,
 } from '@mui/material';
 
-import useAPI from '../../hooks/useAPI';
+import { TextFieldWithLabel } from '../../components';
 
-import Form from '../../components/Form';
-import './Login.css';
+import useAPI from '../../hooks/useAPI';
 
 const INITIAL_CREDENTIALS = { email: '', password: '' };
 
 const Login = () => {
+  const [isAspirantSignup, setIsApirantSignup] = useState(true);
   const [credentials, setCredentials] = useState(INITIAL_CREDENTIALS);
-  const [loginPersonal, setLoginPersonal] = useState(true);
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+  const { palette } = useTheme();
+
+  const canLogin = useCallback(() => {
+    const { email, password } = credentials;
+
+    let isValidSignup = true;
+    if (!email || !password) isValidSignup = false;
+
+    return isValidSignup;
+  }, [credentials]);
 
   const api = useAPI();
 
-  const handleChange = (event) => {
+  const handleCredentialsChange = (event) => {
     const { name, value } = event.target;
 
     setCredentials((prevCredentials) => ({
@@ -31,72 +47,156 @@ const Login = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    const { email, password } = credentials;
     let response;
-    if (loginPersonal) {
-      response = await api.aspirant.login(credentials.email, credentials.password);
-    } else {
-      response = await api.company.login(credentials.email, credentials.password);
-      localStorage.setItem('idCompany', response.company.id);
+    try {
+      if (isAspirantSignup) {
+        response = await api.aspirant.login(email, password);
+      } else {
+        response = await api.company.login(email, password);
+        localStorage.setItem('idCompany', response.company.id);
+      }
+
+      localStorage.setItem('idToken', response.idToken);
+      navigate('/');
+    } catch (loginError) {
+      setError(loginError.message);
     }
-    localStorage.setItem('idToken', response.idToken);
   };
 
   return (
-    <main className="login">
-      <Typography variant="h2" className="welcome">
-        {loginPersonal ? 'Encuentra tu trabajo ideal' : 'Encuentra a los mejores ingenieros para tu equipo'}
-      </Typography>
-      <Form
-        title="Bienvenido de vuelta"
-        onSubmit={handleSubmit}
+    <Grid
+      container
+      component="main"
+      alignItems="center"
+      sx={{
+        height: '100vh',
+        background: palette.white,
+        '&:after': {
+          content: '""',
+          position: 'absolute',
+          width: '60vw',
+          height: '100vh',
+          right: '0',
+          background: palette.gray.B,
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          clipPath: 'polygon(70% 0, 100% 0, 100% 100%, 0% 100%)',
+        },
+      }}
+    >
+      <Grid item md={6} display={{ xs: 'none', md: 'flex' }} justifyContent="center" sx={{ zIndex: 1 }}>
+        <Typography variant="h2" component="p" sx={{ fontWeight: 500, width: '640px' }}>
+          {isAspirantSignup ? 'Encuentra tu trabajo ideal' : 'Encuentra a los mejores ingenieros para tu equipo' }
+        </Typography>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={6}
+        display="flex"
+        direction="column"
+        alignItems="center"
+        sx={{ zIndex: 1 }}
       >
-        <Grid item xs={12}>
-          <ButtonGroup fullWidth>
-            <Button
-              variant={loginPersonal ? 'contained' : 'outlined'}
-              onClick={() => setLoginPersonal(true)}
+        <Card elevation={5} sx={{ width: '640px', borderRadius: '12px' }}>
+          <CardContent sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '24px',
+            width: '100%',
+            padding: '40px 40px 0px',
+          }}
+          >
+            <Typography
+              variant="h4"
+              component="h1"
+              sx={{ fontWeight: 500 }}
             >
-              Personal
-            </Button>
-            <Button
-              variant={!loginPersonal ? 'contained' : 'outlined'}
-              onClick={() => setLoginPersonal(false)}
+              Bienvenido de vuelta
+            </Typography>
+            <ButtonGroup
+              variant="contained"
+              fullWidth
+              sx={{
+                boxShadow: 'none',
+                marginBottom: '8px',
+              }}
             >
-              Empresarial
-            </Button>
-          </ButtonGroup>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="email"
-            label="Email"
-            type="email"
-            variant="filled"
-            value={credentials.email}
-            required
+              <Button
+                onClick={() => setIsApirantSignup(true)}
+                variant={isAspirantSignup ? 'contained' : 'outlined'}
+              >
+                Personal
+              </Button>
+              <Button
+                onClick={() => setIsApirantSignup(false)}
+                variant={isAspirantSignup ? 'outlined' : 'contained'}
+              >
+                Empresarial
+              </Button>
+            </ButtonGroup>
+
+            <TextFieldWithLabel
+              label="¿Cuál es tu correo electrónico?"
+              id="email"
+              type="email"
+              name="email"
+              variant="filled"
+              placeholder="Ejemplo: tu@correo.com"
+              required
+              fullWidth
+              value={credentials.email}
+              onChange={handleCredentialsChange}
+            />
+
+            <TextFieldWithLabel
+              label="¿Cuál es tu contraseña?"
+              id="password"
+              type="password"
+              name="password"
+              variant="filled"
+              placeholder="Escribe tu contraseña"
+              required
+              fullWidth
+              value={credentials.password}
+              onChange={handleCredentialsChange}
+            />
+
+          </CardContent>
+          <Button
+            variant="contained"
+            disableElevation
             fullWidth
-            onChange={handleChange}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            name="password"
-            label="Password"
-            type="password"
-            variant="filled"
-            value={credentials.password}
-            required
-            fullWidth
-            onChange={handleChange}
-          />
-        </Grid>
-        <div className="buttons">
-          <Button variant="text" onClick={() => setCredentials(INITIAL_CREDENTIALS)}>Borrar</Button>
-          <Button variant="contained" sx={{ margin: '0 0 0 1rem' }} type="submit">Ingresar</Button>
-        </div>
-      </Form>
-    </main>
+            disabled={!canLogin()}
+            onClick={handleSubmit}
+            sx={{
+              fontSize: '24px',
+              padding: '16px',
+              marginTop: '40px',
+            }}
+          >
+            COMENZAR
+          </Button>
+          {error && (
+          <Grid item xs={12}>
+            <Alert severity="error">{error}</Alert>
+          </Grid>
+          )}
+        </Card>
+        <Button
+          variant="text"
+          onClick={() => navigate('/signup')}
+          sx={{
+            color: palette.blue.lightest,
+            marginTop: '24px',
+            textDecoration: 'underline',
+          }}
+        >
+          TODAVÍA NO TENGO CUENTA
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
