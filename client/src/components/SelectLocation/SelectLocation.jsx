b8497d29cbb8a59ce3acd8972c9ae4291a5515c9
CristianCazares <cm_cjavier@hotmail.com>
2022-06-08 11:25:43 -0600

    feat(client/views/AspirantSearch): Created multiple select location

---
+++ client/src/components/SelectLocation/SelectLocation.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Grid } from '@mui/material';
+import SelectWithIcon from '../SelectWithIcon/SelectWithIcon';
+import { LocationOn } from '@mui/icons-material';
+
+const INITIAL_LOCATION = {
+    country: '',
+    state: '',
+    city: '',
+}
+
+//UNFINISHED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const SelectLocation = () => {
+  //[location, setLocation] = useState(INITIAL_LOCATION)
+  return (
+    <>
+      <Grid item xs={4}>
+        <SelectWithIcon
+          label={"País"}
+          itemsarray={["a", "b", "c"]}
+          value={""}
+          icon={<LocationOn />}
+          sx={{ backgroundColor: '#E7EDF3' }}
+          fullwidth
+          />
+      </Grid>
+      <Grid item xs={4}>
+        <SelectWithIcon
+          label={"Estado"}
+          itemsarray={["a", "b", "c"]}
+          value={""}
+          icon={<LocationOn />}
+          sx={{ backgroundColor: '#E7EDF3' }}
+          fullwidth
+          disabled
+          />
+      </Grid>
+      <Grid item xs={4}>
+        <SelectWithIcon
+          label={"Ciudad"}
+          itemsarray={["a", "b", "c"]}
+          value={""}
+          icon={<LocationOn />}
+          sx={{ backgroundColor: '#E7EDF3' }}
+          disabled
+          fullwidth
+        />
+      </Grid>
+    </>
+
+  )
+}
+
+export default SelectLocation
