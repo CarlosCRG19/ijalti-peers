e0c00eaa58aba593dbe5f33e78f00b24be8aeca8
BrandonMagana <magana.brandon12@outlook.com>
2022-06-16 13:30:02 -0500

    feat(server/utils): add new util functions: getIdsFromNewObjArr, createNewObjArr

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -5,3 +5,27 @@ export const numArr2ObjArr = (input: number[]): any[] => {
     })    
     return newArray;
 } 
+
+export const createNewObjArr = (input: any[], model: any) => { 
+    const newObjArr = input.map( async (object: any) => {
+        try{
+            const newWorkExpirience = model.create(object);
+            await newWorkExpirience.save();
+            return newWorkExpirience;
+        }catch(error){
+            console.log(error);
+        }
+    })
+    return newObjArr;
+}
+
+export const getIdsFromNewObjArr = (input: any[], model: any) => { 
+    const newObjArr = createNewObjArr(input, model); 
+    const idsFromObjArr = Promise.all(newObjArr)
+    .then((objects) => 
+        objects.map((object: any) => {
+            return {"id": object.id}
+        })
+    )
+    return idsFromObjArr;
+}
