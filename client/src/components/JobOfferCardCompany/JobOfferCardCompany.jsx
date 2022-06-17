fbc05150f332cf72d06d78eeb4c81d1fe03049e6
CristianCazares <cm_cjavier@hotmail.com>
2022-06-16 20:00:41 -0600

    feat(client/JobOfferCardCompany): Added delete button to job offers

--- client/src/components/JobOfferCardCompany/JobOfferCardCompany.jsx
+++ client/src/components/JobOfferCardCompany/JobOfferCardCompany.jsx
@@ -56,6 +56,7 @@ const JobOfferCardCompany = ({
   requiredSkills,
   preferredSkills,
   sxCard,
+  onDelete,
 }) => {
   const [expand, setExpand] = useState(true);
   const [expandMsg, setExpandMsg] = useState('Ver más');
@@ -112,7 +113,12 @@ const JobOfferCardCompany = ({
             </Link>
           )}
           action={(
-            <Button onClick={handleOpen}>Aspirantes Interesados</Button>
+            <>
+              <Button onClick={handleOpen}>Aspirantes Interesados</Button>
+              <Box display="flex" justifyContent="end">
+                <Button variant="text" color='error' onClick={() => onDelete(id)}>Eliminar</Button>
+              </Box>
+            </>
           )}
           subheader={parseDateYYYYMMDD(date)}
         />
